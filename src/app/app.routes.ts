0dfe75186dbe7a87983d73871b57ceaa30c187aa
Miguel <mmedinpi1983@gmail.com>
2026-06-14 01:55:09 -0500

    modulo atencion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Layout } from './shared/components/layout/layout';
 import { Pacientes } from './Pages/pacientes/pacientes';
+import { Atencion } from './Pages/atencion/atencion';
 
 
 export const routes: Routes = [
 
    { path: '', component: Layout, children:[
       {path:'pacientes', component: Pacientes},
+      { path: 'atencion/:idCita', component: Atencion },
    ]}
 ];
