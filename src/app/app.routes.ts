51e34374a020b2eb8e17aa2972f49a1efe7e0e63
Dylan <u23214088@gmail.com>
2026-06-14 01:48:23 -0500

    modulo de citas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Layout } from './shared/components/layout/layout';
-import { Pacientes } from './pages/pacientes/pacientes';
+import { Pacientes } from './Pages/pacientes/pacientes';
+import { Citas } from './Pages/citas/citas';
 
 export const routes: Routes = [
 
    { path: '', component: Layout, children:[
       {path:'pacientes', component: Pacientes},
+      { path: 'citas', component: Citas }, 
    ]}
 ];
