2c71fdbaa9613c2d9edbab294fa090fb1373ccee
Miguel <mmedinpi1983@gmail.com>
2026-06-14 01:40:04 -0500

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/components/layout/layout';
+import { Pacientes } from './pages/pacientes/pacientes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   { path: '', component: Layout, children:[
+      {path:'pacientes', component: Pacientes},
+   ]}
+];
